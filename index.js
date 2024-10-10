284bb53dd3fd77691c4d39860e936dab05ec3ff6
DanikQA <olkhovskyi0204@gmail.com>
2024-10-10 23:44:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let a = Number(prompt('Enter number'));
+
+if ((a % 3 === 0) && (a % 5 === 0)) {
+    alert ('FizzBuzz');
+    } else if (a % 5 === 0) {
+      alert ('Buzz'); 
+        } else if (a % 3 === 0) {
+            alert ('Fizz'); 
+            } else {
+            }
+    
